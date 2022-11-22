09ab29616fe2208d4c98223a532a0f9f0b4790ca
Takashi OGAWA <ogw.tttt@gmail.com>
2022-11-22 17:55:57 +0900

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TYPELINE Util
 // @namespace    http://tampermonkey.net/
-// @version      0.2.0
+// @version      0.2.1
 // @description  TYPELINEの挙動を変えるためのスクリプト
 // @author       ogw.tttt@gmail.com
 // @include      https://preview.n*v.co.jp/*
@@ -133,7 +133,7 @@ td.tlutil-note {
   if (location.host !== 'dashboard.media.play.jp') {
     const testStyleEl = document.createElement('style');
     testStyleEl.appendChild(document.createTextNode(`
-header.pmpui-top_nav,header.pmpui-global-header {
+header.pmpui-top_nav,header.pmpui-global-header,.pmpui-global-menu {
   background-color: darkviolet;
 }
 `));
